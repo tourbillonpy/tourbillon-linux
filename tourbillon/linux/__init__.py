fae81f92ffb5c847e2c43a8d97cb3b257d259413
Francesco Faraone <ffaraone@gaa9.com>
2015-08-26 10:10:48 +0200

    back port to python 2.7 up to 3.3

--- tourbillon/linux/__init__.py
+++ tourbillon/linux/__init__.py
@@ -1,1 +1,8 @@
-from .linux import *
+import sys
+
+PY34_PLUS = sys.version_info[0] == 3 and sys.version_info[1] >= 4
+
+if PY34_PLUS:
+    from .linux.linux import *
+else:
+    from .linux2.linux import *
